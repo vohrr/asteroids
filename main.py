1335582ba29141c2a75eeeb8f7f3008a90c382e5
vohrr <matthew.d.pingel@gmail.com>
2024-12-09 10:18:09 -0500

    initial commit, added main, constants and requirements

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import *
+
+def main(): 
+    print("Starting asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
