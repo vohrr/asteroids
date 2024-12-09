30dce4579ad105361b24daa54a3d0e2f16e9958e
vohrr <matthew.d.pingel@gmail.com>
2024-12-09 10:41:28 -0500

    added game loop function to main, updated gitignore

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from constants import *
 
 def main(): 
     print("Starting asteroids!")
+    pygame.init()
+    pygame.display.set_caption('Asteroids')
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
+    
+    # 3 common steps for the game loop:
+    # check for player inputs
+    while True:
+        
+        # draw the game to the screen
+
+        screen.fill('black')
+        # update the game world
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
