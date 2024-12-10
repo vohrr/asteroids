181c24eecc7f3972b15b63730e53ab5ea2ef49cd
vohrr <matthew.d.pingel@gmail.com>
2024-12-10 08:06:31 -0500

    added first base class

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ def main():
     print("Starting asteroids!")
     pygame.init()
     pygame.display.set_caption('Asteroids')
-    
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Screen width: " + str(SCREEN_WIDTH))
-    print("Screen height: " + str(SCREEN_HEIGHT))
+    run = True 
     
     # 3 common steps for the game loop:
     # check for player inputs
-    while True:
-        
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = not run
         # draw the game to the screen
 
         screen.fill('black')
         # update the game world
         pygame.display.flip()
+        dt += clock.tick(60)
 
 
 if __name__ == "__main__":
